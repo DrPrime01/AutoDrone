f4c9efd483c4ef5b8cec71e55a8ab1bd487c14a6
Gabriel Shoyombo <gabrielshoyombo2002@gmail.com>
2022-09-13 04:24:05 +0100

    Installed Bootstrap

---
+++ pages/home.js
@@ -0,0 +1,10 @@
+// const navbarToggle = document.querySelector("#navbar-toggle")
+// const navbar = document.querySelector("#navbarSupportedContent")
+
+// navbarToggle.addEventListener("click", function() {
+//     if (navbar.classList.contains("animated-dropdown")) {
+//         navbar.classList.remove("animated-dropdown")
+//         } else {
+//             navbar.classList.add("animated-dropdown")
+//             }
+// })
